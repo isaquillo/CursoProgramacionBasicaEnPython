340208527a24505925d38c36f041e13bfb75dd7a
isaquillo <isaaclarios78@gmail.com>
2024-09-21 20:55:26 -0600

    Editing operadores.py

--- EjerciciosClase/05_operadores.py
+++ EjerciciosClase/05_operadores.py
@@ -13,83 +13,88 @@ Clasificación de operadores:
   
 """
 
-# ------------------------- OPERADORES ARITMÉTICOS ----------------------------------------------
-# Descripción: Utilizados para realizar operaciones matemáticas.
-a = 10
-b = 3
+# # ------------------------- OPERADORES ARITMÉTICOS -------------------
+# # Descripción: Utilizados para realizar operaciones matemáticas.
+# a = 10
+# b = 2
 
-suma = a + b              # Suma
-resta = a - b             # Resta
-multiplicacion = a * b    # Multiplicación
-division = a / b          # División (Devuelve el resultado de una división)
-division_entera = a // b  # División (Devuelve el resultado entero de una división)
-modulo = a % b            # Módulo (Devuelve el residuo de una división)
-potencia = a ** b         # Potencia
+# suma = a + b              # Suma
+# resta = a - b             # Resta
+# multiplicacion = a * b    # Multiplicación
+# division = a / b          # División (Devuelve el resultado de una división)
+# division_entera = a // b  # División (Devuelve el resultado entero de una división)
+# modulo = a % b            # Módulo (Devuelve el residuo de una división entera)
+# potencia = a ** b         # Potencia
 
-print("OPERADORES ARITMÉTICOS\n") # La eapresión "\n" permite hacer un salto de línea al imprimir
-print("Suma:", suma)
-print("Resta:", resta)
-print("Multiplicación:", multiplicacion)
-print("División: ", division)
-print("Tipo de dato división: " + str(tbpe(division)))     # Conversión de tipos (casting)
-print("División entera:", division_entera)
-print("Tipo de dato división entera: " + str(tbpe(division_entera))) # Aquí no hay casting
-print("Módulo:", modulo)
-print("Potencia:", potencia)
+# print("OPERADORES ARITMÉTICOS\n") # La expresión "\n" permite hacer un salto de línea al imprimir
+
+# print(type(a))
+# print(type(b))
+# print("Con a = " + str(a) + " y b = " + str(b) + "\n")
+# print("Suma:", suma)
+# print("Resta:", resta)
+# print("Multiplicación:", multiplicacion)
+# print("División: ", division)
+# print("Tipo de dato división: " + str(type(division)))     # Conversión de tipos (casting)
+# print("División entera:", division_entera)
+# print("Tipo de dato división entera: " + str(type(division_entera))) # Aquí no hay casting
+# print("Módulo:", modulo)
+# print("Potencia:", potencia)
+
+# # --------------------------OPERADORES DE COMPARACIÓN -------------------
+# # Descripción: Comparan dos valores y devuelven un valor booleano (True o False).
+
+# a = 5
+# b = 10
+
+# print("OPERADORES DE COMPARACIÓN\n")
+# print("Con a = " + str(a) + " y b = " + str(b) + "\n")
+
+# print(a == b)  # Igual
+# print(a != b)  # Diferente
+# print(a > b)   # Mayor
+# print(a < b)   # Menor
+# print(a >= b)  # Mayor o igual
+# print(a <= b)  # Menor o igual
 
 
-# --------------------------OPERADORES DE COMPARACIÓN -------------------------------------------
-# Descripción: Comparan dos valores y devuelven un valor booleano (True o False).
-
-a = 5
-b = 10
-print("OPERADORES DE COMPARACIÓN\n")
-print("Con a = " + str(a) + " y b = " + str(b) + "\n")
-print("a == b: " + str(a == b))  # Igual
-print("a != b: " + str(a != b))  # Diferente
-print("a > b: " + str(a > b))   # Mayor
-print("a < b: " + str(a < b))   # Menor
-print("a >= b: " + str(a >= b))  # Mayor o igual
-print("a <= b: " + str(a <= b))  # Menor o igual
-
-
-
-# ------------------------------ OPERADORES LÓGICOS -----------------------------------------------
+# ------------------------------ OPERADORES LÓGICOS ---------------------
 # Descripción: Combinan operadores booleanos.
 
 a = True
 b = False
 
-print("OPERADORES BOOLEANOS\n")
-print("Con a = " + str(a) + " y b = " + str(b) + "\n")
-print("a and b: " + str(a and b))  # b (Devuelve True sólo si ambas variables son verdaderas)
-print("a or b: " + str(a or b))   # O (Devuelve True cuando cualquiera de las variables es verdadera)
-print("not a: " + str(not a))    # Negación (Cambia el valor de la expresión booleana donde se aplica el operador)
+# print("OPERADORES BOOLEANOS\n")
+# print(a and b) # (Devuelve True sólo si ambas variables son verdaderas)
+# print(a or b) #  (Devuelve True cuando cualquiera de las variables es verdadera)
+# print(not a) # Negación (Cambia el valor de la expresión booleana donde se aplica el operador)
+    
+# # Se debe tener cuidado al determinar dónde poner este operador
+# print(a or b) 
+# print(not a or b)
+# print(a and not b)
+# print(not(a or b))
 
-# Se debe tener cuidado al determinar dónde poner este operador
-print("a or b: " + str(a or b)) # Resultado: 
-print("not a or b: " + str(not a or b)) # Resultado: 
-print("a or not b: " + str(a or not b)) # Resultado :
-print("not (a and b): " + str(not(a or b))) # Resultado: 
-
-# Podemos agrupar expresiones y aplicarle operadores booleanos
-a = 3
-b = 5
-resultado = (a < 3) and (b < a)
-print("(a < 3) and (b < a): " + str(resultado))
-otro_resultado = (True or False) and (True and True)
-print("(True or False) and (True and True): " + str(otro_resultado))
+# # Podemos agrupar expresiones y aplicarle operadores booleanos
+# a = 3
+# b = 5
+# print((a < 3) and (b < a))
+# print((True or False) and (True and True))
 
 
 
-# ---------------------------- OPERADORES DE ASIGNACIÓN ----------------------------------------
-# Descripción: Asignan valores a variables.
+# # ---------------------------- OPERADORES DE ASIGNACIÓN ----------------------------------------
+# # Descripción: Asignan valores a variables.
 
-c = 5 # Asignación simple
-c += 2  # c = c + 2 (Asignación con operadores aritméticos)
-print(c)  # 7 (Ahora c tiene el valor de 7)
-c *= 3  # c = c * 3
-print(c)  # 21
+# c = 5 # Asignación simple
+# c += 2  # c = c + 2 (Asignación con operadores aritméticos)(Ahora c tiene el valor de 7)
+# print(c)  # 7 
+# c *= 3  # c = c * 3
+# print(c)  # 21
+# c /= 3 # c = c / 3
+# print(c)
+# c %= 3 # c = c % 3
+# print(c)
 
 
 
@@ -100,9 +105,10 @@ print(c)  # 21
 lista = [1, 2, 3]
 print(1 in lista)    # Pertenencia
 print(4 not in lista)  # No pertenece
+print(5 in lista)
 
 a = [1, 2, 3]
-b = a
+b = a # referencia
 z = [1, 2, 3]
 print(a is b)  # Identidad (True)
 print(a is z)  # Identidad (False)

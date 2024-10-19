0fd00c8fb3c9b3a577c17a2ea66470cfcb6dc1c2
Isaac Larios <isaaclarios78@gmail.com>
2024-10-19 10:44:47 -0600

    Cambios en repaso.py

--- EjerciciosClase/07_repaso.py
+++ EjerciciosClase/07_repaso.py
@@ -1,21 +1,21 @@
 
-# # Tarea 4
+# Tarea 4
 
-# # for i in range(10,20,2):
-# #     print(i)
+for i in range(10,20,2):
+    print(i)
 
 
-# num1 = 2
-# num2 = 4
+num1 = 2
+num2 = 4
 
-# # print(num1 + num2)
+# print(num1 + num2)
 
-# palabra1 = "2"
-# palabra2 = " Mundo"
+palabra1 = "2"
+palabra2 = " Mundo"
 
-# # print(palabra1 + palabra2)
+# print(palabra1 + palabra2)
 
-# print(num1 + int(palabra1))
+print(num1 + int(palabra1))
 
 
 edades = [20, -3, -200, 7, 45, 1, 16, 432, 175, 60, 55, 8, 77, 28, 21, 45, 16]
@@ -46,3 +46,36 @@ for edad in edades:
             print ("Adulto Mayor")
     else:
         print("Edad fuera del rango válido")
+
+#  Escribe un programa que imprima los primeros 10 números naturales que sean múltiplos de 3
+
+# Opción 1
+for i in range(0,30,3):
+    print(i)
+
+# Opción 2
+contador = 0
+while(contador < 10):
+    print(contador * 3)
+    contador += 1
+
+
+"""
+ Dada la siguiente lista de palabras, escribe un programa que 
+ imprima cuántas letras tiene cada palabra.  Ojo: No se permite 
+ imprimir directamente el número de letras, el programa tiene 
+ que calcular cuántas letres tiene.
+
+"""
+palabras = ["Tazo", "Dorado", "Becado", "Coqueto"]
+
+# Opción 1
+for palabra in palabras:
+    print(palabra + " - " + str(len(palabra)))
+
+# Opción 2
+for palabra in palabras:
+    contador = 0
+    for letra in palabra:
+        contador += 1
+    print(palabra + " - " + str(contador))
